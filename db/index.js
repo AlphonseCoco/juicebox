72c782e111c39969138ed07ae33303e629d74c6f
AlphonseCoco <alphonsecoco@gmail.com>
2022-10-01 11:16:19 -0500

    ssl inclusion

--- db/index.js
+++ db/index.js
@@ -1,6 +1,11 @@
 const { Client } = require('pg'); //imports pg module
 
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
+const client = new Client(
+    {connectionString: 
+        process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
+        ssl: process.env.NODE_ENV ==='production' ? { rejectUnauthorized: false } : undefined,
+    }
+    );
 
 
 
